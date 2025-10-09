ba69b4584656245878ca677b0f95b2a02c4fd4b5
ThomasKsK <thomaspois1707@gmail.com>
2025-10-09 19:36:37 +0200

    ajout de l'aspect utilisateur

---
+++ src/main/java/com/littlebook/entity/UserEntity.java
@@ -0,0 +1,73 @@
+package com.littlebook.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "uuid", updatable = false, nullable = false)
+    private UUID uuid;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "email_address", nullable = false, unique = true)
+    private String emailAddress;
+
+    @Column(name = "profile_creation_date", nullable = false)
+    private LocalDate profileCreationDate = LocalDate.now();
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    // Getters & Setters
+    public UUID getUuid() {
+        return uuid;
+    }
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getProfileCreationDate() {
+        return profileCreationDate;
+    }
+    public void setProfileCreationDate(LocalDate profileCreationDate) {
+        this.profileCreationDate = profileCreationDate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
