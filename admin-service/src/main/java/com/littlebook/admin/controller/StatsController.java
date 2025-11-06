5d60615450c6c10379f6047f848e4c92618cae17
Justine Kosinski <justine.kosinski@yahoo.fr>
2025-11-06 11:50:07 +0100

    update routes and stats

--- admin-service/src/main/java/com/littlebook/admin/controller/StatsController.java
+++ admin-service/src/main/java/com/littlebook/admin/controller/StatsController.java
@@ -1,5 +1,8 @@
 package com.littlebook.admin.controller;
 
+import com.littlebook.admin.dto.LoginRecordRequest;
+import com.littlebook.admin.dto.SetLastLoginRequest;
+import com.littlebook.admin.dto.SetTotalLoginsRequest;
 import com.littlebook.admin.entity.LoginEvent;
 import com.littlebook.admin.entity.ReviewActivity;
 import com.littlebook.admin.entity.UserLoginStats;
@@ -46,5 +49,34 @@ public class StatsController {
     @GetMapping("/summary")
     public Map<String, Object> summary() {
         return statsService.summary();
+    }
+
+    @GetMapping("/users/enriched")
+    public List<Map<String, Object>> listUsersEnriched() {
+        return statsService.listUsersEnriched();
+    }
+
+    @PostMapping("/users/{id}/login")
+    public ResponseEntity<UserLoginStats> recordLogin(@PathVariable UUID id, @RequestBody LoginRecordRequest req) {
+        UserLoginStats updated = statsService.recordLogin(id, req);
+        return ResponseEntity.ok(updated);
+    }
+
+    @PatchMapping("/users/{id}/increment")
+    public ResponseEntity<UserLoginStats> incrementLogin(@PathVariable UUID id) {
+        UserLoginStats updated = statsService.incrementLogin(id);
+        return ResponseEntity.ok(updated);
+    }
+
+    @PatchMapping("/users/{id}/last-login")
+    public ResponseEntity<UserLoginStats> setLastLogin(@PathVariable UUID id, @RequestBody SetLastLoginRequest req) {
+        UserLoginStats updated = statsService.setLastLogin(id, req);
+        return ResponseEntity.ok(updated);
+    }
+
+    @PatchMapping("/users/{id}/total-logins")
+    public ResponseEntity<UserLoginStats> setTotalLogins(@PathVariable UUID id, @RequestBody SetTotalLoginsRequest req) {
+        UserLoginStats updated = statsService.setTotalLogins(id, req);
+        return ResponseEntity.ok(updated);
     }
 }
