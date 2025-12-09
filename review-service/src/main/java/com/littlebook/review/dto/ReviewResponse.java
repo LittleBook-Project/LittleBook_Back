82ae7abf19580bfaa012125c520698b236d730d8
MOUNIAT-1002 <20225680@etud.univ-evry.fr>
2025-12-09 15:45:36 +0100

    dev service review et coordination de données entre servies

---
+++ review-service/src/main/java/com/littlebook/review/dto/ReviewResponse.java
@@ -0,0 +1,48 @@
+package com.littlebook.review.dto;
+
+import java.time.LocalDate;
+
+public class ReviewResponse {
+
+    private Long id;
+    private String description;
+    private LocalDate reviewCreationDate;
+    private Integer rating;
+    private String userUuid;
+    private String bookIsbn;
+
+    public ReviewResponse() {
+    }
+
+    public ReviewResponse(Long id,
+                          String description,
+                          LocalDate reviewCreationDate,
+                          Integer rating,
+                          String userUuid,
+                          String bookIsbn) {
+        this.id = id;
+        this.description = description;
+        this.reviewCreationDate = reviewCreationDate;
+        this.rating = rating;
+        this.userUuid = userUuid;
+        this.bookIsbn = bookIsbn;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public LocalDate getReviewCreationDate() { return reviewCreationDate; }
+    public void setReviewCreationDate(LocalDate reviewCreationDate) { this.reviewCreationDate = reviewCreationDate; }
+
+    public Integer getRating() { return rating; }
+    public void setRating(Integer rating) { this.rating = rating; }
+
+    public String getUserUuid() { return userUuid; }
+    public void setUserUuid(String userUuid) { this.userUuid = userUuid; }
+
+    public String getBookIsbn() { return bookIsbn; }
+    public void setBookIsbn(String bookIsbn) { this.bookIsbn = bookIsbn; }
+}
